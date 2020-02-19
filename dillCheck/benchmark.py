9b016b2f0681a4d8c166ef31e17a3f3b8cb88bed
JacobO1 <jacob1996@hotmail.dk>
2020-02-19 12:09:47 +0100

    checkpointing with Dill works perfectly. Now comes the fun part

--- dillCheck/benchmark.py
+++ dillCheck/benchmark.py
@@ -6,7 +6,15 @@ import dill
 import sys
 
 bench = util.Benchmark("Solving the heat equation using the jacobi method", "height*width*iterations")
+
+#Global variables needed to save and resume program
 counter = 0
+H = bench.args.size[0]
+W = bench.args.size[0]
+I = 29000
+
+grid = bench.load_data()
+
 
 def init_grid(height, width, dtype=np.float32):
     grid = np.zeros((height + 2, width + 2), dtype=dtype)
@@ -17,7 +25,7 @@ def init_grid(height, width, dtype=np.float32):
     return grid
 
 
-def jacobi(grid, epsilon=0.005, max_iterations=None):
+def jacobi(grid, max_iterations, epsilon=0.005):
     def loop_body(grid):
         global counter
         center = grid[1:-1, 1:-1]
@@ -31,8 +39,6 @@ def jacobi(grid, epsilon=0.005, max_iterations=None):
         bench.plot_surface(grid, "2d", 0, 200, -200)
         counter += 1
         print(counter)
-        if (counter == 22):
-            dill.dump_session('bch.pkl')
 
 #DEBUG PRINTS
 #        print('grid:\n {}'.format(grid))
@@ -59,22 +65,18 @@ def jacobi(grid, epsilon=0.005, max_iterations=None):
 
 
 def main():
-    H = bench.args.size[0]
-    W = bench.args.size[0]
-    I = 10000
-
-    grid = bench.load_data()
-    if grid is not None:
-        grid = grid['grid']
-    else:
-        grid = init_grid(H, W, dtype=bench.dtype)
-
+    global H, W, I, grid
+    
+    if grid is None:
+    	grid = init_grid(H, W, dtype=bench.dtype)
+    
     bench.start()
     grid = jacobi(grid, max_iterations=I)
     bench.stop()
     bench.save_data({'grid': grid})
     bench.pprint()
-
+    dill.dump_session('bch.pkl')
+    np.savetxt('yesCheck', grid)
 
 if __name__ == "__main__":
     main()
